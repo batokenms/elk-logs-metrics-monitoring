18c078077d968812ca1f4f7b0481675dbedc86f7
joshking1 <88409463+joshking1@users.noreply.github.com>
2023-10-05 17:24:34 -0400

    Update elk.sh

--- elk.sh
+++ elk.sh
@@ -2,28 +2,28 @@
 
 # Create namespace 
 
-kubectl apply -f josh.yaml -n josh
+kubectl apply -f josh.yaml -n dev
 
 # elasticsearch-ss Deployment 
 
-kubectl apply -f elasticsearch-ss.yaml -n josh 
+kubectl apply -f elasticsearch-ss.yaml -n dev 
 
 # logstash-deployment Deployment 
 
-kubectl apply -f logstash-deployment.yaml -n josh
+kubectl apply -f logstash-deployment.yaml -n dev
 
 # filebeat-ds Deployment
 
-kubectl apply -f filebeat-ds.yaml -n josh
+kubectl apply -f filebeat-ds.yaml -n dev
 
 # metricbeat-ds Deployment 
 
-kubectl apply -f metricbeat-ds.yaml -n josh
+kubectl apply -f metricbeat-ds.yaml -n dev
 
 # kibana-deployment Deployment
 
-kubectl apply -f kibana-deployment.yaml -n josh
+kubectl apply -f kibana-deployment.yaml -n dev
 
 # Deployment curator-cronjob
 
-kubectl apply -f curator-cronjob.yaml -n josh
+kubectl apply -f curator-cronjob.yaml -n dev
